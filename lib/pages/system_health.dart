6c99923b5a13a004c74228d1efcff3bbbc87724f
Yaash Jain <yaash.jain@alumni.ubc.ca>
2021-05-12 23:30:59 -0700

    Add fetch button to System Health page
    - Simple placeholder that increments a counter upon button press

--- lib/pages/system_health.dart
+++ lib/pages/system_health.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mup_app/backend/mup_firebase.dart';
 
 class SystemHealthPage extends StatefulWidget {
   @override
@@ -6,12 +7,36 @@ class SystemHealthPage extends StatefulWidget {
 }
 
 class _SystemHealthPageState extends State<SystemHealthPage> {
+  final mupFirestore = new MupFirestore();
+
+  int _counter = 0;
+
+  void _setData() {
+    _counter++;
+  }
+
+  void onPressed() {
+    setState(() {
+      _setData();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('System Health')),
       body: Center(
-        child: Text('This is the system health monitor page'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(padding: EdgeInsets.all(10.0), child: Text("$_counter")),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                child:
+                    ElevatedButton(onPressed: onPressed, child: Text('Fetch'))),
+          ],
+        ),
       ),
     );
   }
