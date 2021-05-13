d6a3771263fc75f709e9bf0dd5d6584ad7736a7a
Yaash Jain <yaash.jain@alumni.ubc.ca>
2021-05-12 22:14:50 -0700

    Add useful comment explaining the insert funciton

--- lib/backend/mup_firebase.dart
+++ lib/backend/mup_firebase.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 final databaseReference = FirebaseFirestore.instance;
 
+// The insertion function returns a future that needs to be resolved by the caller
 Future<DocumentReference> insertObjectToFirestore(
         String collectionName, Map<String, dynamic> data) =>
     databaseReference.collection(collectionName).add(data);
