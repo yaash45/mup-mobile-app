ad7a677c20aa98a5cbdfbde15e5048706bf448b0
Yaash Jain <yaash.jain@alumni.ubc.ca>
2021-06-06 16:30:44 -0700

    Improve Add New Device Page
    - Make input boxes similar to rest of app
    - Handle cases where required input is not specified

--- lib/pages/add_new_device.dart
+++ lib/pages/add_new_device.dart
@@ -22,12 +22,11 @@ class _AddNewDevicePageState extends State {
     nameHolder.clear();
   }
 
-  void _showToast(BuildContext context) {
+  void _showToast(BuildContext context, String message) {
     final scaffold = ScaffoldMessenger.of(context);
     scaffold.showSnackBar(
       SnackBar(
-        content: Text(
-            "Added device with IMEI = $_imei, Serial = $_serial, Name = $_name"),
+        content: Text(message),
         action: SnackBarAction(
             label: 'OK', onPressed: scaffold.hideCurrentSnackBar),
       ),
@@ -36,21 +35,33 @@ class _AddNewDevicePageState extends State {
 
   void _addDevice() {
     setState(() {
-      Map<String, dynamic> deviceData = {
-        "user": "test",
-        "imei": _imei,
-        "serial": _serial,
-        "name": _name,
-        "timestamp": DateTime.now().millisecondsSinceEpoch,
-      };
+      if (_imei == '') {
+        _showToast(context, "Please enter IMEI number for your device");
+      } else if (_serial == '') {
+        _showToast(context, "Please enter Serial number for your device");
+      } else {
+        if (_name == '') {
+          // TODO: Get device count for account and set name accordingly
+          _name = 'mangoh-1';
+        }
 
-      // Push device info to firebase
-      mupFirestore.insertObject('devices', deviceData).then((value) {
-        print(value.id);
-        clearTextInput();
-        _showToast(context);
-        Navigator.pop(context);
-      });
+        Map<String, dynamic> deviceData = {
+          "user": "test",
+          "imei": _imei,
+          "serial": _serial,
+          "name": _name,
+          "timestamp": DateTime.now().millisecondsSinceEpoch,
+        };
+
+        // Push device info to firebase
+        mupFirestore.insertObject('devices', deviceData).then((value) {
+          print(value.id);
+          clearTextInput();
+          _showToast(context,
+              "Added device with IMEI = $_imei, Serial = $_serial, Name = $_name");
+          Navigator.pop(context);
+        });
+      }
     });
   }
 
@@ -82,33 +93,72 @@ class _AddNewDevicePageState extends State {
           padding: EdgeInsets.all(10.0),
           child: Container(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
+                Spacer(
+                  flex: 1,
+                ),
                 TextField(
-                  decoration: InputDecoration(hintText: "IMEI"),
+                  decoration: InputDecoration(
+                    labelText: "IMEI",
+                    hintText: "Please check your device for IMEI",
+                    hintStyle: TextStyle(color: Colors.grey[500]),
+                    border: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.blue, width: 2.0),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
                   keyboardType: TextInputType.number,
                   controller: imeiHolder,
                   onChanged: _setImei,
                 ),
+                Spacer(
+                  flex: 1,
+                ),
                 TextField(
-                  decoration: InputDecoration(hintText: "Serial Number"),
+                  decoration: InputDecoration(
+                    labelText: "Serial Number",
+                    hintText: "Please check your device for Serial No.",
+                    hintStyle: TextStyle(color: Colors.grey[500]),
+                    border: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.blue, width: 2.0),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
                   keyboardType: TextInputType.number,
                   controller: serialHolder,
                   onChanged: _setSerial,
                 ),
+                Spacer(
+                  flex: 1,
+                ),
                 TextField(
                   decoration: InputDecoration(
-                    hintText: "Device name (optional)",
+                    labelText: "Device name (optional)",
+                    hintText: "This is your device nickname",
+                    hintStyle: TextStyle(color: Colors.grey[500]),
+                    border: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.blue, width: 2.0),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
                   ),
                   keyboardType: TextInputType.name,
                   controller: nameHolder,
                   onChanged: _setName,
                 ),
-                Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: ElevatedButton(
-                      onPressed: _addDevice,
-                      child: Text("Add"),
-                    ))
+                Spacer(
+                  flex: 10,
+                ),
+                ElevatedButton(
+                  onPressed: _addDevice,
+                  child: Text("Add"),
+                ),
+                Spacer(
+                  flex: 2,
+                ),
               ],
             ),
           )),
