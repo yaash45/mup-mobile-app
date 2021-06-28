6e27363d7d1231872751e286a75bf4fd841c418b
Yaash Jain <yaash.jain@alumni.ubc.ca>
2021-06-28 10:56:44 -0700

    Use current user state on my account page

--- lib/pages/my_account.dart
+++ lib/pages/my_account.dart
@@ -1,6 +1,9 @@
 //import 'package:beautiful_list/model/lesson.dart';
 import 'package:flutter/material.dart';
+import 'package:mup_app/states/CurrentUser.dart';
 import 'package:mup_app/templates/appbar.dart';
+import 'package:mup_app/templates/colors.dart';
+import 'package:provider/provider.dart';
 
 //import 'package:beautiful_list/detail_page.dart';
 class MyAccount extends StatefulWidget {
@@ -11,71 +14,50 @@ class MyAccount extends StatefulWidget {
 }
 
 class _MyAccountState extends State<MyAccount> {
-  List lessons;
-
   @override
   Widget build(BuildContext context) {
-    ListTile makeListTile() => ListTile(
-          contentPadding:
-              EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-          leading: Container(
-            padding: EdgeInsets.only(right: 12.0),
-            decoration: new BoxDecoration(
+    var _currentUser = Provider.of<CurrentUser>(context, listen: false);
+
+    ListTile makeListTile(String title, String value) => ListTile(
+          leading: SizedBox(
+            width: 90,
+            child: Container(
+              padding: EdgeInsets.only(right: 12.0),
+              decoration: new BoxDecoration(
                 border: new Border(
-                    right: new BorderSide(width: 1.0, color: Colors.grey))),
-            child: //Icon(Icons.account_box, color: Colors.white),
-                Text("Username", style: TextStyle(fontWeight: FontWeight.bold)),
+                  right: new BorderSide(width: 1.0, color: Colors.grey),
+                ),
+              ),
+              child: //Icon(Icons.account_box, color: Colors.white),
+                  Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+            ),
           ),
           title: Text(
-            "John Doe",
+            value,
             style:
-                TextStyle(color: Colors.grey[700], fontWeight: FontWeight.w300),
-          ),
-          // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
-
-          /*    subtitle: Row(
-            children: <Widget>[
-              Expanded(
-                  flex: 1,
-                  child: Container(
-                    // tag: 'hero',
-                    child: LinearProgressIndicator(
-                        backgroundColor: Color.fromRGBO(209, 224, 224, 0.2),
-                        value: 0.33,
-                        valueColor: AlwaysStoppedAnimation(Colors.green)),
-                  )),
-              Expanded(
-                flex: 4,
-                child: Padding(
-                    padding: EdgeInsets.only(left: 10.0),
-                    child: Text("21",
-                        style: TextStyle(color: Colors.white))),
-              )
-            ],
-          ), */
-          trailing:
-              Icon(Icons.keyboard_arrow_right, color: Colors.grey, size: 30.0),
-        );
-
-    Card makeCard() => Card(
-          elevation: 15.0,
-          margin: new EdgeInsets.symmetric(horizontal: 5.0, vertical: 3.0),
-          child: Container(
-            decoration: BoxDecoration(color: Colors.white),
-            child: makeListTile(),
+                TextStyle(color: Colors.grey[700], fontWeight: FontWeight.w400),
           ),
         );
 
-    final makeBody = Container(
-      // decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, 1.0)),
-      child: ListView.builder(
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        itemCount: 3,
-        itemBuilder: (BuildContext context, int index) {
-          return makeCard();
-        },
-      ),
+    Card makeCard(String title, String value) => Card(
+        elevation: 2,
+        shadowColor: MupColors.shadowColor,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(width: 0.5, color: Colors.blueAccent),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [makeListTile(title, value)],
+        ));
+
+    final makeBody = ListView(
+      padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        makeCard("Full Name", _currentUser.fullName),
+        makeCard("Email", _currentUser.email),
+        makeCard("Account created", _currentUser.accountCreated),
+      ],
     );
 
     final topAppBar = MupAppBar('My Account');
